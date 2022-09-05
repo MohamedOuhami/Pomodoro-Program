ca840073bd7ac0f2aac6e2cf7096bd4e457f9a86
Mohamed Ouhami <mohamed.ouhami2001@gmail.com>
2022-09-05 17:34:02 +0100

    Setup the Work states and Music tracks
    Setup the Pomodoro timer with 2 states ( Working / Rest )
    After each lap, you hear a ring to indicate the next state

---
+++ Timer.py
@@ -0,0 +1,53 @@
+# Setting up the timer. Calculating the remaining time
+
+import time, datetime
+from playsound import playsound
+
+# Setting up the work state
+work = True
+rest = False
+
+
+# Creating a function that acts as countdown
+def pomodoroCountdown(m,s):
+    #Calculating the number of seconds given
+    total_seconds = m * 60 + s
+
+    #While loop checks if the countdown reached zero
+    while total_seconds > 0 :
+
+        #Remaining seconds
+        # It gives you the differences between the moment you started the program and the final countdown and It changes iT to the form of hours
+        timer = datetime.timedelta(seconds= total_seconds)
+
+        # Print the time left
+        print(timer, end="\r")
+
+        # Delaying the program by 1 seconds
+        time.sleep(1)
+
+        # Reduce 1 second
+        total_seconds -= 1
+    
+    #print("Changing state")
+    
+# Work State
+def workState(work_state,rest_state):
+   
+    if(work_state == True):
+        playsound('/home/mohamed/Documents/Adonis Programming/PomodoroTimerProgram/Work.wav')
+        print("Start Working")
+        pomodoroCountdown(50, 0)
+        work_state = False
+        rest_state = True
+
+    if(rest_state == True):
+        playsound('/home/mohamed/Documents/Adonis Programming/PomodoroTimerProgram/Rest.wav')
+        print("You can rest now")
+        pomodoroCountdown(10, 0)
+        work_state = True
+        rest_state = False
+
+
+while(True):
+    workState(work, rest)
